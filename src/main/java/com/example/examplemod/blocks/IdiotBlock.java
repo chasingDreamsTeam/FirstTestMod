a264b98bad7624f78af1c876686c516c29f8687d
Hang  Gao <hang.gao@student.manchester.ac.uk>
2021-07-10 23:11:25 +0800

    add new light property

--- src/main/java/com/example/examplemod/blocks/IdiotBlock.java
+++ src/main/java/com/example/examplemod/blocks/IdiotBlock.java
@@ -1,18 +1,27 @@
 package com.example.examplemod.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.IntegerProperty;
 
 import java.util.function.ToIntFunction;
 
 public class IdiotBlock extends Block {
 
+    public static ToIntFunction<BlockState> lightLevel = BlockState -> 15;
+
     public IdiotBlock() {
+
         super(Properties.of(Material.METAL)
                 .sound(SoundType.METAL)
                 .strength(2.0f)
+                .lightLevel(lightLevel)
         );
+
+
         setRegistryName("idiotblock");
+
     }
 }
