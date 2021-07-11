e74e4748e3ef05c330cb3617ec99a403e690b781
Hang  Gao <hang.gao@student.manchester.ac.uk>
2021-07-11 17:21:26 +0800

    change firstBlock texture

--- src/main/java/com/example/examplemod/blocks/IdiotBlock.java
+++ src/main/java/com/example/examplemod/blocks/IdiotBlock.java
@@ -4,8 +4,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.registries.ObjectHolder;
 
+import javax.annotation.Nullable;
 import java.util.function.ToIntFunction;
 
 public class IdiotBlock extends Block {
@@ -21,7 +30,23 @@ public class IdiotBlock extends Block {
         );
 
 
-        setRegistryName("idiotblock");
+        this.setRegistryName("idiotblock");
 
+    }
+
+    @Override
+    public void setPlacedBy(World world, BlockPos pos, BlockState state, @Nullable LivingEntity entity, ItemStack stack){
+        if(entity != null){
+            world.setBlock(pos, state.setValue(BlockStateProperties.FACING, getFacingFromEntity(pos, entity)), 2);
+        }
+    }
+
+    public static Direction getFacingFromEntity(BlockPos clickedBlock, LivingEntity entity){
+        return Direction.getNearest((float) (entity.getX() - clickedBlock.getX()),(float) (entity.getY() - clickedBlock.getY()),(float) (entity.getZ() - clickedBlock.getZ()));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder){
+        builder.add(BlockStateProperties.FACING);
     }
 }
